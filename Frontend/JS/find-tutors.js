e963e346f13dd8d771db84abcfd3de4e63e21247
Eba Adisu <ebaadisu2@gmail.com>
2024-01-29 13:07:08 +0300

    tutor

--- Frontend/JS/find-tutors.js
+++ Frontend/JS/find-tutors.js
@@ -1,33 +1,41 @@
 const displayTutorsBtn = document.getElementById('displayTutorsBtn');
 const tutorsContainer = document.getElementById('tutorsContainer');
+const token = localStorage.getItem('token') || 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwibmFtZSI6InRlYWNoIiwicm9sZSI6Ikluc3RydWN0b3IiLCJpYXQiOjE3MDY1MjE3MDksImV4cCI6MTcwOTExMzcwOX0.v8EPXb2j_X3qCZrAKCj-_n5f13wiVo5FWrbmOQLkJG4'
 console.log(tutorsContainer.innerHTML);
 displayTutorsBtn.addEventListener('click', async (event) => { 
     event.preventDefault();
     const selectedCourse = document.getElementById('courseSelect').value;
     if (selectedCourse !== '') {
-        const response = await fetch(`http://localhost:3000/api/v1/instructors?course=${selectedCourse}`, {
-            method: 'GET',
-            headers: {
-                'Content-Type': 'application/json',
-                'authorization': 'Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJJZCI6MSwibmFtZSI6InRlYWNoIiwicm9sZSI6Ikluc3RydWN0b3IiLCJpYXQiOjE3MDY1MTk1ODEsImV4cCI6MTcwOTExMTU4MX0.kGgWIFqKNZubWkuhwGv_eWlA1vQmi8stKa95-pmIEcE'
-
-            },
-        });
-        const data = await response.json();
-        // console.log(data)
-        const tutors = data.map(tutor => {
-            return `<div class="flex items-center">
+        try {
+            const response = await fetch(`http://localhost:3000/api/v1/instructors?course=${selectedCourse}`, {
+                method: 'GET',
+                headers: {
+                    'Content-Type': 'application/json',
+                    'authorization': token
+                },
+            });
+            const data = await response.json();
+            // console.log(data)
+            if (response.status !== 200) {
+                throw new Error(data.msg);
+            }
+            const tutors = data.map(tutor => {
+                return `<div class="flex items-center">
                 <div class="w-16 h-16 bg-black mr-4 rounded-full"></div>
                 <div class="ml-20">
                     <h3 class="text-lg font-bold">${tutor.name}</h3>
                     <p>Expertise: ${selectedCourse}</p>
                     <p>Username: ${tutor.name}</p>
-                    <p>Average Rating: ${tutor.rate}</p>
-                    <p>Contact Info: Email: ${tutor.email}</p>
+                    <p>Average Rating: ${tutor.rate.toFixed(1)}</p>
+                    <p>Contact Info: Email: <a href= 'mailto:${tutor.email}'>${tutor.email}</a></p>
                 </div>
             </div>
             `
-        });
-        tutorsContainer.innerHTML = tutors.join('');
+            });
+            tutorsContainer.innerHTML = tutors.join('');
+
+        } catch (error) {
+            alert(error.message);
+        }
     }
 })
